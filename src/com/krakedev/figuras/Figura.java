64ea2001ba0004b2b94f86b8b5ce441ba5a9fc45
Alexandra Ramirez <133824521+alexandraR-Code@users.noreply.github.com>
2026-09-24 16:44:27 -0500

    Parte 1: estructura base Figura, Cuadrado, Triangulo

---
+++ src/com/krakedev/figuras/Figura.java
@@ -0,0 +1,32 @@
+package com.krakedev.figuras;
+
+public class Figura {
+	//Atributos
+	private String nombre;
+	private String color;
+		
+	//Métodos getter y setter
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	//Método toString
+	@Override
+	public String toString() {
+		return "Figura [nombre=" + nombre + ", color=" + color + "]";
+	}
+	
+	
+	
+	
+	
+
+}
